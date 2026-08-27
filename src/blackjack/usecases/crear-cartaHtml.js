c592cc446c4f11afbf9f0daf2290d745dd67491c
Anthony <anthonyluisperez3@gmail.com>
2026-08-27 07:16:45 -0500

    ffix: corregido las rutas de las cartas

--- src/blackjack/usecases/crear-cartaHtml.js
+++ src/blackjack/usecases/crear-cartaHtml.js
@@ -10,7 +10,7 @@ export const crearCartaHTML = (carta) => {
     if(!carta) throw new Error('La carta ess un argumento obligatorio');
 
     const imgCarta = document.createElement('img');
-    imgCarta.src = `./assets/cartas/${ carta }.png`; //3H, JD
+    imgCarta.src = `./cartas/${ carta }.png`; //3H, JD
     imgCarta.classList.add('carta');
 
     return imgCarta;
