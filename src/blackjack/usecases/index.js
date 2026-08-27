bf8d718f6c8f9d61e19609fab6ee4e2d7bb1d7d8
Anthony <anthonyluisperez3@gmail.com>
2026-08-27 06:28:24 -0500

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,7 @@
+
+export {crearDeck} from './crear-deck.js';
+export {pedirCarta} from './pedir-carta.js';
+export {valorCarta} from './valor-carta.js';
+export {turnoComputadora} from './turno-computador.js';
+export {crearCartaHTML} from './crear-cartaHtml.js'
+
---
+++ src/blackjack/usecases/valor-carta.js
@@ -0,0 +1,15 @@
+// pedirCarta();
+/**
+ * 
+ * @param {Array<String>} carta obtener el valor de la carta
+ * @returns {Number} valor de carta 
+ */
+
+
+export const valorCarta = ( carta ) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ? 
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+}
